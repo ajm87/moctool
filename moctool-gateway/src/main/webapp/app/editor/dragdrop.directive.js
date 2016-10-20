61f522914b381a65ccd986c74dc692de38629f19
Andy Morgan <ajm87@bath.ac.uk>
2016-10-20 16:56:04 +0100

    Change DB provider to MySQL

--- moctool-gateway/src/main/webapp/app/editor/dragdrop.directive.js
+++ moctool-gateway/src/main/webapp/app/editor/dragdrop.directive.js
@@ -4,67 +4,29 @@ Draggable.$inject = ['$document', '$window'];
 function Draggable($document, $window){
   function makeDraggable(scope, element, attrs) {
     var original = false;
-
-    $(element).mousedown(function() {
-      original = true;
-    });
+    //TODO: Rewrite
+      $(element).mousedown(function() {
+        original = true;
+      });
 
       $(element).draggable({
         helper: "clone",
         revert: "invalid"
       });
-$( "#droponme" ).droppable({
-    drop: function( event, ui ) {
-        if(original){
-             ui.helper.removeClass('ui-draggable-dragging');
-             var newDiv = $(ui.helper).clone().removeClass('ui-draggable-dragging');
-             newDiv.draggable({
-               revert: "invalid"
-             });
-             $(this).append(newDiv);
-             original = false;
-        }
-      }  
-});
-    }
-  // function makeDraggable(scope, element, attr) {
-  //   var startX = 0;
-  //   var startY = 0;
-
-  //   // get element starting coords
-  //   var x = element[0].getBoundingClientRect().left;
-  //   var y = element[0].getBoundingClientRect().top;
-
-  //   element.css({
-  //     position: 'absolute',
-  //     cursor: 'pointer'
-  //   });
-
-  //   element.on('mousedown', function(e) {
-  //     e.preventDefault();
-  //     startX = e.pageX - x;
-  //     startY = e.pageY - y;
-
-  //     $document.on('mousemove', mousemove);
-  //     $document.on('mouseup', mouseup);
-  //   });
-
-  //   function mousemove(e) {
-  //     y = e.pageY - startY;
-  //     x = e.pageX - startX;
-
-  //     element.css({
-  //       top: y + 'px',
-  //       left: x + 'px'
-  //     });
-  //   }
-
-  //   function mouseup() {
-  //     $document.unbind('mousemove', mousemove);
-  //     $document.unbind('mouseup', mouseup);
-  //   }
-  // }
-  return {
+      $( "#droponme" ).droppable({
+          drop: function( event, ui ) {
+              if(original){
+                  ui.helper.removeClass('ui-draggable-dragging');
+                  var newDiv = $(ui.helper).clone().removeClass('ui-draggable-dragging');
+                  newDiv.draggable({
+                    revert: "invalid"
+                  });
+                  $(this).append(newDiv);
+                  original = false;
+              }
+            }  
+      });
+  } return {
     link: makeDraggable
   };
 }
